7c010715658d7e6ac38cb48b20b34dee7de219a1
samyak-chhe2020 <72869523+samyak-chhe2020@users.noreply.github.com>
2020-11-27 09:04:15 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,24 @@
+class Box {
+    constructor(x,y,width,height)
+    {
+        var options = {isStatic:true}
+    
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width = width;
+        this.height= height; 
+
+        World.add(world,this.body);
+    }
+
+     display(){
+            var pos=this.body.position;
+            var angle=this.body.angle;
+            Push();
+            translate(pos.x,pos.y);
+            Rotate(angle)
+            rectMode(CENTER);
+            Fill("red");
+            rect(0,0,this.width,this.height);
+            Pop();
+        }
+}
